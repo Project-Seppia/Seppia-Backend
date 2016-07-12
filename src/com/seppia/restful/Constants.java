7c8d807a0fb1e8f92fa934ca54d86014ea492cce
a56097634a <a56097634a@gmail.com>
2016-07-12 14:37:58 -0700

    Update mysql on aws res server.

--- src/com/seppia/restful/Constants.java
+++ src/com/seppia/restful/Constants.java
@@ -3,8 +3,8 @@ package com.seppia.restful;
 //Change these parameters according to DB
 public class Constants {
   public static String dbClass = "com.mysql.jdbc.Driver";
-  private static String dbName= "seppiatest";
-  public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+  private static String dbName= "ebdb";
+  public static String dbUrl = "jdbc:mysql://aai8i1td9ah0qi.c2jlvu9axx5p.us-east-1.rds.amazonaws.com:3306/"+dbName;
   public static String dbUser = "root";
-  public static String dbPwd = "seppia";
+  public static String dbPwd = "seppiadb";
 }
