067dc5fa905517e543ffe76a85e8ba994456db45
a56097634a <a56097634a@gmail.com>
2016-07-11 20:14:47 -0700

    implement db part of the insert and get all locations

--- src/com/seppia/restful/Location.java
+++ src/com/seppia/restful/Location.java
@@ -1,7 +1,26 @@
 package com.seppia.restful;
 
 public class Location {
-	private String address1;
+	private String address;
 	private String phone;
 	private String name;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/com/seppia/restful/LocationDB.java
+++ src/com/seppia/restful/LocationDB.java
@@ -1,5 +1,74 @@
 package com.seppia.restful;
 
-public class LocationDB {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
+import com.seppia.restful.Location;
+
+public class LocationDB {
+	
+	//Get all locations
+	public ArrayList<Location> getLocations(Connection connection) throws Exception{
+		ArrayList<Location> locationData = new ArrayList<Location>();
+		try
+		{
+			PreparedStatement ps = connection.prepareStatement("SELECT address,phone,name FROM locations ORDER BY id DESC");
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+			Location locationObject = new Location();
+			locationObject.setAddress(rs.getString("address"));
+			locationObject.setPhone(rs.getString("phone"));
+			locationObject.setName(rs.getString("name"));
+			locationData.add(locationObject);
+			}
+			return locationData;
+		}
+		catch(Exception e){
+			throw e;
+		}
+	}
+	
+	public ArrayList<Location> getLocationByAddress(Connection connection, String address) throws Exception{
+		ArrayList<Location> locationData = new ArrayList<Location>();
+		try
+		{
+			PreparedStatement ps = connection.prepareStatement("SELECT address,phone,name FROM locations WHERE address = ?");
+			ps.setString(1, address);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+			Location locationObject = new Location();
+			locationObject.setAddress(rs.getString("address"));
+			locationObject.setPhone(rs.getString("phone"));
+			locationObject.setName(rs.getString("name"));
+			locationData.add(locationObject);
+			}
+			return locationData;
+		}
+		catch(Exception e){
+			throw e;
+		}
+	}
+	
+	public boolean InsertLocation(Connection connection, String address, String phone, String name) throws Exception{
+		boolean insertStatus = false;
+		try
+		{
+			PreparedStatement ps = connection.prepareStatement("INSERT into locations(address, phone, name) values(?, ?, ?)");
+			ps.setString(1, address);
+			ps.setString(2, phone);
+			ps.setString(3, name);
+			int status = ps.executeUpdate();
+			if(status>0){
+				insertStatus = true;
+			}
+			return insertStatus;
+		}
+		catch(Exception e){
+			throw e;
+		}
+	}
 }
