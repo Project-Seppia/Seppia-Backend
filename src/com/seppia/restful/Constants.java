15e0d91cc44c60f5734b7356512c1fec08e9f21c
a56097634a <a56097634a@gmail.com>
2016-07-14 21:03:35 -0700

    finalize the environment and make it a working copy online

--- src/com/seppia/restful/Constants.java
+++ src/com/seppia/restful/Constants.java
@@ -4,7 +4,7 @@ package com.seppia.restful;
 public class Constants {
   public static String dbClass = "com.mysql.jdbc.Driver";
   private static String dbName= "ebdb";
-  public static String dbUrl = "jdbc:mysql://aai8i1td9ah0qi.c2jlvu9axx5p.us-east-1.rds.amazonaws.com:3306/"+dbName;
+  public static String dbUrl = "jdbc:mysql://aa1mcncj2hmom9q.c2jlvu9axx5p.us-east-1.rds.amazonaws.com:3306/"+dbName;
   public static String dbUser = "root";
   public static String dbPwd = "seppiadb";
 }
